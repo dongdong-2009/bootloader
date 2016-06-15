0c1123e4b8e4b86e593dee4714006892f6ab19cc
masuchen <654681279@qq.com>
2016-06-15 10:23:10 +0800

    将与功率板的通讯口改为UART5

--- BSP/UART/uart1.c
+++ BSP/UART/uart1.c
@@ -5,6 +5,7 @@
 #include "stm32f10x_gpio.h"
 #include "misc.h"
 #include "stdio.h"
+#include "string.h"
 
 u8 u1_buffer[2048];
 
@@ -18,16 +19,17 @@ void usart1_conf(u32 baud_rate)
     USART_InitTypeDef   USART_InitStructure;
     NVIC_InitTypeDef NVIC_InitStructure;
 
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1 | RCC_APB2Periph_GPIOA | RCC_APB2Periph_AFIO, ENABLE);
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_UART5, ENABLE);
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC|RCC_APB2Periph_GPIOD,ENABLE);
 
-    GPIO_InitStructure.GPIO_Pin =  GPIO_Pin_9;
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+    GPIO_Init(GPIOD,&GPIO_InitStructure);
+
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12;
     GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-    GPIO_Init(GPIOA, &GPIO_InitStructure);
-
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10;
-    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
-    GPIO_Init(GPIOA, &GPIO_InitStructure);
+    GPIO_Init(GPIOC,&GPIO_InitStructure);
 
     USART_InitStructure.USART_BaudRate = baud_rate;
     USART_InitStructure.USART_WordLength = USART_WordLength_8b;
@@ -35,13 +37,12 @@ void usart1_conf(u32 baud_rate)
     USART_InitStructure.USART_Parity = USART_Parity_No;
     USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
     USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
-    USART_Init(USART1, &USART_InitStructure);
-    USART_ITConfig(USART1, USART_IT_RXNE, ENABLE);
-    USART_Cmd(USART1, ENABLE);
+    USART_Init(UART5, &USART_InitStructure);
 
-//	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_0);
+    USART_ITConfig(UART5, USART_IT_RXNE, ENABLE);
+    USART_Cmd(UART5, ENABLE);
 
-    NVIC_InitStructure.NVIC_IRQChannel = USART1_IRQn;
+    NVIC_InitStructure.NVIC_IRQChannel = UART5_IRQn;
     NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
     NVIC_InitStructure.NVIC_IRQChannelSubPriority = 1;
     NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
@@ -49,33 +50,22 @@ void usart1_conf(u32 baud_rate)
 }
 
 
-/////重定向c库函数printf到USART1
-//int fputc(int ch, FILE *f)
-//{
-//    /* 发送一个字节数据到USART1 */
-//    USART_SendData(USART1, (uint8_t) ch);
-
-//    /* 等待发送完毕 */
-//    while (USART_GetFlagStatus(USART1, USART_FLAG_TXE) == RESET);
-
-//    return (ch);
-//}
 
 
 static void USART1_SEND_CHAR(char ch)
 {
-    USART_SendData(USART1, ch);
-    while (USART_GetFlagStatus(USART1, USART_FLAG_TXE) == RESET);
+    USART_SendData(UART5, ch);
+    while (USART_GetFlagStatus(UART5, USART_FLAG_TXE) == RESET);
 }
 
 
-void USART1_IRQHandler(void)
+void UART5_IRQHandler(void)
 {
-    if (USART_GetITStatus(USART1, USART_IT_RXNE) != RESET)
+    if (USART_GetITStatus(UART5, USART_IT_RXNE) != RESET)
         {
 
-            u8 dat = USART_ReceiveData(USART1);
-            
+            u8 dat = USART_ReceiveData(UART5);
+
 //            USART_ClearITPendingBit(USART1, USART_IT_RXNE);
             if(1==is_slave )
                 {
@@ -88,9 +78,9 @@ void USART1_IRQHandler(void)
                                     u1_buffer[u1_bufferindex++]=dat;
                                     return ;
                                 }
-                                    is_slave=0;
-                                    receive_slave=1;
-                                    u1_buffer[u1_bufferindex++]=dat;
+                            is_slave=0;
+                            receive_slave=1;
+                            u1_buffer[u1_bufferindex++]=dat;
                         }
                     else
                         {
@@ -114,7 +104,7 @@ void MASTER_SEND(u8 * buf,u32 len)
             USART1_SEND_CHAR(*buf++);
         }
 }
-#include "string.h"
+
 
 void before_send_sa(void)
 {
